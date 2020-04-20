21bdc8ee0e3d9b3a451b852f58f7fd8fabfcd1f7
BorisBorsh <tihomirov_tm@mail.ru>
2020-04-20 17:58:49 +0300

    color year by year div growth companies func implemented, stock_analyser is now functioning

--- analyse_and_highlight_data.py
+++ analyse_and_highlight_data.py
@@ -77,6 +77,7 @@ def color_fundamental_parameters_of_companies_in_list(companies_list, champ_work
 def get_champ_list_after_5years_dividends_increase_in_row_analysis(input_champ_list, eval_model, hist_worksheet):
     """Analysing dividends magnification for 5 years in row"""
 
+    print("Analysing post preliminary champions list")
     ws_hist = hist_worksheet
     company_hist_list_start_indx = first_company_in_list_cell(ws_hist)
     company_hist_list_end_indx = last_company_in_hist_list_cell(ws_hist)
@@ -118,8 +119,10 @@ def color_params_of_champ_list_5years_dividends_increase_in_row(input_champ_list
 
 
 # Year by year dividend growth
-def get_final_champ_list_after_year_by_year_div_growth_analisys(input_champ_list, hist_worksheet):
+def get_final_champ_list_after_year_by_year_div_growth_analysis(input_champ_list, hist_worksheet):
     """"""
+
+    print("Analysing final champions list")
     ws_hist = hist_worksheet
     company_hist_list_start_indx = first_company_in_list_cell(ws_hist)
     company_hist_list_end_indx = last_company_in_hist_list_cell(ws_hist)
@@ -154,6 +157,44 @@ def get_final_champ_list_after_year_by_year_div_growth_analisys(input_champ_list
             champ_array_year_by_year_div_growth.append(input_champ_list[company])
 
     return champ_array_year_by_year_div_growth
+
+
+def color_champ_list_after_year_by_year_div_growth_analysis(input_champ_list, hist_worksheet, color_fill):
+    """"""
+
+    ws_hist = hist_worksheet
+    company_hist_list_start_indx = first_company_in_list_cell(ws_hist)
+    company_hist_list_end_indx = last_company_in_hist_list_cell(ws_hist)
+
+    for company in range(0, len(input_champ_list)):
+        i = find_company_in_list(input_champ_list[company]['company_name'], ws_hist, company_hist_list_start_indx,
+                                 company_hist_list_end_indx)
+
+        ws_hist['A' + str(i)].fill = color_fill
+        ws_hist['C' + str(i)].fill = color_fill
+        ws_hist['D' + str(i)].fill = color_fill
+        ws_hist['E' + str(i)].fill = color_fill
+        ws_hist['F' + str(i)].fill = color_fill
+        ws_hist['G' + str(i)].fill = color_fill
+        ws_hist['H' + str(i)].fill = color_fill
+        ws_hist['I' + str(i)].fill = color_fill
+        ws_hist['J' + str(i)].fill = color_fill
+        ws_hist['K' + str(i)].fill = color_fill
+        ws_hist['L' + str(i)].fill = color_fill
+        ws_hist['M' + str(i)].fill = color_fill
+        ws_hist['N' + str(i)].fill = color_fill
+        ws_hist['O' + str(i)].fill = color_fill
+        ws_hist['P' + str(i)].fill = color_fill
+        ws_hist['Q' + str(i)].fill = color_fill
+        ws_hist['R' + str(i)].fill = color_fill
+        ws_hist['S' + str(i)].fill = color_fill
+        ws_hist['T' + str(i)].fill = color_fill
+        ws_hist['U' + str(i)].fill = color_fill
+        ws_hist['Y' + str(i)].fill = color_fill
+        ws_hist['V' + str(i)].fill = color_fill
+        ws_hist['W' + str(i)].fill = color_fill
+        ws_hist['X' + str(i)].fill = color_fill
+
 
 """def binary_search(company_name, worksheet, first_indx, last_indx):
     low = first_indx
--- stock_analyser.py
+++ stock_analyser.py
@@ -4,7 +4,9 @@ from operations_on_companies_list import *
 from analyse_and_highlight_data import get_champ_list_after_fundamental_analysis, \
                                 color_fundamental_parameters_of_companies_in_list, \
                                 get_champ_list_after_5years_dividends_increase_in_row_analysis, \
-                                color_params_of_champ_list_5years_dividends_increase_in_row
+                                color_params_of_champ_list_5years_dividends_increase_in_row, \
+                                get_final_champ_list_after_year_by_year_div_growth_analysis, \
+                                color_champ_list_after_year_by_year_div_growth_analysis
 from openpyxl import load_workbook
 from openpyxl.styles import GradientFill
 from openpyxl.styles import Font
@@ -33,70 +35,19 @@ color_fundamental_parameters_of_companies_in_list(prelim_champs_list, ws, blue_g
 post_prelim_champs_list = get_champ_list_after_5years_dividends_increase_in_row_analysis(prelim_champs_list,
                                                                                          stnd_model, ws_hist)
 #Color fill all the cells that met conditions of 5 years in row of dividend increase
-get_champ_list_after_5years_dividends_increase_in_row_analysis(post_prelim_champs_list, ws_hist, green_grad_bg_fill)
-#
 color_params_of_champ_list_5years_dividends_increase_in_row(post_prelim_champs_list, ws_hist, blue_grad_bg_fill)
 
-
-champ_list = post_prelim_champs_list
-
-#Year by year dividend growth
-result_array = []
-for company in range(0, len(champ_list)):
-    i = find_company_in_list(champ_list[company]['company_name'], ws_hist, company_hist_list_start_indx, company_hist_list_end_indx)
-
-    if (
-        ws_hist['C' + str(i)].font.color is None
-        and ws_hist['D' + str(i)].font.color is None
-        and ws_hist['E' + str(i)].font.color is None
-        and ws_hist['F' + str(i)].font.color is None
-        and ws_hist['G' + str(i)].font.color is None
-        and ws_hist['H' + str(i)].font.color is None
-        and ws_hist['I' + str(i)].font.color is None
-        and ws_hist['K' + str(i)].font.color is None
-        and ws_hist['L' + str(i)].font.color is None
-        and ws_hist['N' + str(i)].font.color is None
-        and ws_hist['O' + str(i)].font.color is None
-        and ws_hist['P' + str(i)].font.color is None
-        and ws_hist['Q' + str(i)].font.color is None
-        and ws_hist['R' + str(i)].font.color is None
-        and ws_hist['S' + str(i)].font.color is None
-        and ws_hist['T' + str(i)].font.color is None
-        and ws_hist['U' + str(i)].font.color is None
-        and ws_hist['V' + str(i)].font.color is None
-        and ws_hist['W' + str(i)].font.color is None
-        and ws_hist['X' + str(i)].font.color is None
-        and ws_hist['Y' + str(i)].font.color is None
-    ):
-        result_array.append(champ_list[company])
-        ws_hist['A' + str(i)].fill = green_grad_bg_fill
-        ws_hist['C' + str(i)].fill = green_grad_bg_fill
-        ws_hist['D' + str(i)].fill = green_grad_bg_fill
-        ws_hist['E' + str(i)].fill = green_grad_bg_fill
-        ws_hist['F' + str(i)].fill = green_grad_bg_fill
-        ws_hist['G' + str(i)].fill = green_grad_bg_fill
-        ws_hist['H' + str(i)].fill = green_grad_bg_fill
-        ws_hist['I' + str(i)].fill = green_grad_bg_fill
-        ws_hist['J' + str(i)].fill = green_grad_bg_fill
-        ws_hist['K' + str(i)].fill = green_grad_bg_fill
-        ws_hist['L' + str(i)].fill = green_grad_bg_fill
-        ws_hist['M' + str(i)].fill = green_grad_bg_fill
-        ws_hist['N' + str(i)].fill = green_grad_bg_fill
-        ws_hist['O' + str(i)].fill = green_grad_bg_fill
-        ws_hist['P' + str(i)].fill = green_grad_bg_fill
-        ws_hist['Q' + str(i)].fill = green_grad_bg_fill
-        ws_hist['R' + str(i)].fill = green_grad_bg_fill
-        ws_hist['S' + str(i)].fill = green_grad_bg_fill
-        ws_hist['T' + str(i)].fill = green_grad_bg_fill
-        ws_hist['U' + str(i)].fill = green_grad_bg_fill
-        ws_hist['V' + str(i)].fill = green_grad_bg_fill
-        ws_hist['W' + str(i)].fill = green_grad_bg_fill
-        ws_hist['X' + str(i)].fill = green_grad_bg_fill
-        ws_hist['Y' + str(i)].fill = green_grad_bg_fill
-
-champ_list = result_array
+#Get a final list of champions according analysis of year by year div growth
+final_champ_list = get_final_champ_list_after_year_by_year_div_growth_analysis(post_prelim_champs_list, ws_hist)
+#Color parameters of year by year div growth with blue fill
+color_champ_list_after_year_by_year_div_growth_analysis(final_champ_list, ws_hist, green_grad_bg_fill)
+#Color parameters of year by year div growth with green fill (for final list)
+color_params_of_champ_list_5years_dividends_increase_in_row(final_champ_list, ws_hist, green_grad_bg_fill)
+#Color all the cells of fundamental parameters fo final champ list
+color_fundamental_parameters_of_companies_in_list(final_champ_list, ws, green_grad_bg_fill)
 
 print("Saving data to a C:\Result.xlsx file")
+
 
 wb.save('C:\\Result.xlsx')
 print("DONE!")
